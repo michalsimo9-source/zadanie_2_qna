a96bf30f1ad9f590c65ff62563601bd5360fd994
Mihal Šimo <michalsimo9@gmail.com>
2026-03-24 12:35:14 +0100

    Oddelenie hlavičky a pätičky do parts

---
+++ kontakt.php
@@ -0,0 +1,40 @@
+<?php require_once "parts/header.php"; ?>
+
+  <main>
+    <section class="banner">
+      <div class="container text-white">
+        <h1>Kontakt</h1>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="col-100 text-center">
+          <p><strong><em>Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui. Mollit deserunt culpa incididunt laborum commodo in culpa.</em
+          ></strong></p>
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <div class="row">
+        <div class="col-50"> 
+          <h3>Máte otázky?</h3>
+          <p>Incididunt mollit quis eiusmod tempor voluptate duis eu enim amet excepteur cupidatat magna velit. </p> 
+          <p>Velit id ad laborum velit commodo.</p>
+          <p>Consectetur laborum aliqua nulla anim cupidatat consectetur est veniam cupidatat.</p>
+        </div>
+        <div class="col-50 text-right">
+          <h3>Napíšte nám</h3>
+          <form id="contact" action="thankyou.html">
+            <input type="text" placeholder="Vaše meno" id ="meno"  required><br>
+            <input type="email" placeholder="Váš email" id="email" required><br>
+            <textarea name="" placeholder="Vaša správa" id="sprava"></textarea><br>
+            <input type="checkbox" name="" id="" required>
+            <label for=""> Súhlasím so spracovaním osobných údajov.</label><br>
+            <input type="submit" value="Odoslať">
+          </form>
+        </div>
+      </div>
+    </section>
+  </main>
+  
+<?php require_once "parts/footer.php"; ?>
---
+++ thankyou.php
@@ -0,0 +1,19 @@
+<?php require_once "parts/header.php"; ?>
+
+    <main>
+      <section class="banner">
+        <div class="container text-white">
+          <h1>Ďakujeme</h1>
+        </div>
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <h2>Ďakujeme za vyplnenie formulára</h2>
+          </div>
+        </div>
+      </section>
+
+    </main>
+    
+<?php require_once "parts/footer.php"; ?>
